1ec80063e2409537ca7add07a84411e6f3cd69f5
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-08-16 18:47:55 +0800

    更新

---
+++ Referencing Assets through Code/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,17 @@
+﻿using CodeMonkey;
+using CodeMonkey.Utils;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("GameLogic.Init");
+
+        CreateSprite(GameAssets.i.gun);
+    }
+    private static void CreateSprite(Sprite sprite)
+    {
+        UtilsClass.CreateWorldSprite("Sprite", sprite, Vector3.zero, new Vector3(20, 20), 0, Color.white);
+    }
+}
